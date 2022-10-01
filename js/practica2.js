1586dadb43db3ad6a192f9a1f7966b42d59a3297
Fran Enguix <enguix.fco@gmail.com>
2022-10-01 14:59:41 +0200

    Normals and uvs added

--- js/practica2.js
+++ js/practica2.js
@@ -158,123 +158,80 @@ function getAntebrazo(material) {
 }
 
 function getPinzaTipGeometry() {
-    // const vertices = [
-    //     // front
-    //     { pos: [-10, -10,  10], norm: [ 0,  0,  1], uv: [0, 1], },
-    //     { pos: [ 10, -10,  10], norm: [ 0,  0,  1], uv: [1, 1], },
-    //     { pos: [-10,  10,  10], norm: [ 0,  0,  1], uv: [0, 0], },
-        
-    //     { pos: [-10,  10,  1], norm: [ 0,  0,  1], uv: [0, 0], },
-    //     { pos: [ 10, -10,  1], norm: [ 0,  0,  1], uv: [1, 1], },
-    //     { pos: [ 10,  10,  1], norm: [ 0,  0,  1], uv: [1, 0], },
-    //     // right
-    //     { pos: [ 1, -1,  1], norm: [ 1,  0,  0], uv: [0, 1], },
-    //     { pos: [ 1, -1, -1], norm: [ 1,  0,  0], uv: [1, 1], },
-    //     { pos: [ 1,  1,  1], norm: [ 1,  0,  0], uv: [0, 0], },
-        
-    //     { pos: [ 1,  1,  1], norm: [ 1,  0,  0], uv: [0, 0], },
-    //     { pos: [ 1, -1, -1], norm: [ 1,  0,  0], uv: [1, 1], },
-    //     { pos: [ 1,  1, -1], norm: [ 1,  0,  0], uv: [1, 0], },
-    //     // back
-    //     { pos: [ 1, -1, -1], norm: [ 0,  0, -1], uv: [0, 1], },
-    //     { pos: [-1, -1, -1], norm: [ 0,  0, -1], uv: [1, 1], },
-    //     { pos: [ 1,  1, -1], norm: [ 0,  0, -1], uv: [0, 0], },
-        
-    //     { pos: [ 1,  1, -1], norm: [ 0,  0, -1], uv: [0, 0], },
-    //     { pos: [-1, -1, -1], norm: [ 0,  0, -1], uv: [1, 1], },
-    //     { pos: [-1,  1, -1], norm: [ 0,  0, -1], uv: [1, 0], },
-    //     // left
-    //     { pos: [-1, -1, -1], norm: [-1,  0,  0], uv: [0, 1], },
-    //     { pos: [-1, -1,  1], norm: [-1,  0,  0], uv: [1, 1], },
-    //     { pos: [-1,  1, -1], norm: [-1,  0,  0], uv: [0, 0], },
-        
-    //     { pos: [-1,  1, -1], norm: [-1,  0,  0], uv: [0, 0], },
-    //     { pos: [-1, -1,  1], norm: [-1,  0,  0], uv: [1, 1], },
-    //     { pos: [-1,  1,  1], norm: [-1,  0,  0], uv: [1, 0], },
-    //     // top
-    //     { pos: [ 1,  1, -1], norm: [ 0,  1,  0], uv: [0, 1], },
-    //     { pos: [-1,  1, -1], norm: [ 0,  1,  0], uv: [1, 1], },
-    //     { pos: [ 1,  1,  1], norm: [ 0,  1,  0], uv: [0, 0], },
-        
-    //     { pos: [ 1,  1,  1], norm: [ 0,  1,  0], uv: [0, 0], },
-    //     { pos: [-1,  1, -1], norm: [ 0,  1,  0], uv: [1, 1], },
-    //     { pos: [-1,  1,  1], norm: [ 0,  1,  0], uv: [1, 0], },
-    //     // bottom
-    //     { pos: [ 1, -1,  1], norm: [ 0, -1,  0], uv: [0, 1], },
-    //     { pos: [-1, -1,  1], norm: [ 0, -1,  0], uv: [1, 1], },
-    //     { pos: [ 1, -1, -1], norm: [ 0, -1,  0], uv: [0, 0], },
-        
-    //     { pos: [ 1, -1, -1], norm: [ 0, -1,  0], uv: [0, 0], },
-    //     { pos: [-1, -1,  1], norm: [ 0, -1,  0], uv: [1, 1], },
-    //     { pos: [-1, -1, -1], norm: [ 0, -1,  0], uv: [1, 0], },
-    // ];
-
-    // const positions = [];
-    // const normals = [];
-    // const uvs = [];
-    // for (const vertex of vertices) {
-    //     positions.push(...vertex.pos);
-    //     normals.push(...vertex.norm);
-    //     uvs.push(...vertex.uv);
-    // }
-
-    // const geometry = new THREE.BufferGeometry();
-    // const positionNumComponents = 3;
-    // const normalNumComponents = 3;
-    // const uvNumComponents = 2;
-    // geometry.setAttribute(
-    //     'position', 
-    //     new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents)
-    // );
-    // geometry.setAttribute(
-    //     'normal',
-    //     new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents)
-    // );
-    // geometry.setAttribute(
-    //     'uv',
-    //     new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents)
-    // );
     const geometry = new THREE.BufferGeometry();
     const positionNumComponents = 3;
-    const vertices = [
-        10, -9.5, -2,
-        10, -9.5, 2,
-        -10, -9.5, -2,
-        -10, -9.5, 2,
+    const normalNumComponents = 3;
+    const uvNumComponents = 2;
+    const newVertices = [
+        // Back face
+        { pos: [ 10, -9.5, -2], norm: [0, -1, 0], uv: [0, 0] }, // 0
+        { pos: [ 10, -9.5,  2], norm: [0, -1, 0], uv: [0, 1] }, // 1
+        { pos: [-10, -9.5, -2], norm: [0, -1, 0], uv: [1, 0] }, // 2
+        { pos: [-10, -9.5,  2], norm: [0, -1, 0], uv: [1, 1] }, // 3
 
-        6, 9.5, 0,
-        6, 9.5, 2,
-        -6, 9.5, 0,
-        -6, 9.5, 2,
-        /*
-        0, 0, 0,
-        19, 8, 0,
-        19, 8, 2,
-        0, 0, 4,
-        19, 12, 2,
-        19, 12, 0,
-        0, 20, 4,
-        0, 20, 0
-        */
+        // Top face
+        { pos: [10, -9.5, -2], norm: [1, 0, 0], uv: [0, 0] },   // 4
+        { pos: [10, -9.5,  2], norm: [1, 0, 0], uv: [0, 1] },   // 5
+        { pos: [10,  9.5,  0], norm: [1, 0, 0], uv: [1, 0] },   // 6
+        { pos: [10,  9.5,  2], norm: [1, 0, 0], uv: [1, 1] },   // 7
+
+        // Bottom face
+        { pos: [-10, -9.5, -2], norm: [-1, 0, 0], uv: [0, 0] }, // 8
+        { pos: [-10, -9.5,  2], norm: [-1, 0, 0], uv: [0, 1] }, // 9
+        { pos: [-10,  9.5,  0], norm: [-1, 0, 0], uv: [1, 0] }, // 10
+        { pos: [-10,  9.5,  2], norm: [-1, 0, 0], uv: [1, 1] }, // 11
+
+        // Front face
+        { pos: [10, 9.5, 0], norm: [0, 1, 0], uv: [0, 0] },     // 12
+        { pos: [10, 9.5, 2], norm: [0, 1, 0], uv: [0, 1] },     // 13
+        { pos: [-10, 9.5, 0], norm: [0, 1, 0], uv: [1, 0] },    // 14
+        { pos: [-10, 9.5, 2], norm: [0, 1, 0], uv: [1, 1] },    // 15
+
+        // Right face
+        { pos: [10, -9.5, 2], norm: [0, 0, 1], uv: [0, 0] },    // 16
+        { pos: [-10, -9.5, 2], norm: [0, 0, 1], uv: [0, 1] },   // 17
+        { pos: [10, 9.5, 2], norm: [0, 0, 1], uv: [1, 0] },     // 18
+        { pos: [-10, 9.5, 2], norm: [0, 0, 1], uv: [1, 1] },    // 19
+
+        // Left face
+        { pos: [10, -9.5, -2], norm: [0, 0, -1], uv: [0, 0] },   // 20
+        { pos: [-10, -9.5, -2], norm: [0, 0, -1], uv: [0, 1] },  // 21
+        { pos: [10, 9.5, 0], norm: [0, 0, -1], uv: [1, 0] },     // 22
+        { pos: [-10, 9.5, 0], norm: [0, 0, -1], uv: [1, 1] },    // 23
     ];
+    const positions = [];
+    const normals = [];
+    const uvs = [];
+    for (const vertex of newVertices) {
+        positions.push(...vertex.pos);
+        normals.push(...vertex.norm);
+        uvs.push(...vertex.uv);
+    }
     geometry.setAttribute(
         'position', 
-        new THREE.BufferAttribute(new Float32Array(vertices), positionNumComponents)
+        new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents)
+    );
+    geometry.setAttribute(
+        'normal', 
+        new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents)
+    );
+    geometry.setAttribute(
+        'uv', 
+        new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents)
     );
     geometry.setIndex([
-        0, 1, 2, 1, 3, 2,   // Back face
-        0, 5, 1, 0, 4, 5,   // Up face
-        3, 7, 2, 7, 6, 2,   // Down face
-        6, 5, 4, 6, 7, 5,   // Front face
-        1, 5, 3, 3, 5, 7,   // Right face
-        2, 4, 0, 2, 6, 4    // Left face
+        0, 1, 2, 1, 3, 2,       // Back face
+        4, 7, 5, 4, 6, 7,       // Top face
+        9, 11, 8, 11, 10, 8,    // Bot face
+        14, 13, 12, 14, 15, 13, // Front face
+        16, 18, 17, 17, 18, 19, // Right face
+        21, 22, 20, 21, 23, 22, // Left face
     ]);
     return geometry;
 }
 
 function getPinza(material) {
     const pinza = new THREE.Mesh(new THREE.BoxGeometry(20, 19, 4, 8), material);
-    // prePinza.position.set(0, 0, 15);
     pinza.rotateX(Math.PI / 2);
     const pinzaTip = getPinzaTip(material);
     pinzaTip.position.set(0, 19, 0);
@@ -283,7 +240,7 @@ function getPinza(material) {
 }
 
 function getPinzaTip(material) {
-    const pinzaTip = new THREE.Mesh(getPinzaTipGeometry(), getNormalMaterial());
+    const pinzaTip = new THREE.Mesh(getPinzaTipGeometry(), material);
     return pinzaTip
 }
 
@@ -305,43 +262,6 @@ function loadScene()
 
     // Floor
     scene.add(getFloor(material));
-
-    // // Esfera y cubo
-    // const esfera = new THREE.Mesh( new THREE.SphereGeometry(1,20,20), material );
-    // const cubo = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), material );
-    // esfera.position.x = 1;
-    // cubo.position.x = -1;
-
-    // esferaCubo = new THREE.Object3D();
-    // esferaCubo.add(esfera);
-    // esferaCubo.add(cubo);
-    // esferaCubo.position.y = 1.5;
-
-    // scene.add(esferaCubo);
-
-    // scene.add( new THREE.AxesHelper(3) );
-    // cubo.add( new THREE.AxesHelper(1) );
-
-    // Modelos importados
-    // const loader = new THREE.ObjectLoader();
-    // loader.load('models/soldado/soldado.json', 
-    // function (objeto)
-    // {
-    //     cubo.add(objeto);
-    //     objeto.position.y = 1;
-    // });
-
-    // const glloader = new GLTFLoader();
-    // glloader.load('models/RobotExpressive.glb',
-    // function(objeto)
-    // {
-    //     esfera.add(objeto.scene);
-    //     objeto.scene.scale.set(0.5,0.5,0.5);
-    //     objeto.scene.position.y = 1;
-    //     objeto.scene.rotation.y = -Math.PI/2;
-    //     console.log("ROBOT");
-    //     console.log(objeto);
-    // });
 }
 
 function update()
